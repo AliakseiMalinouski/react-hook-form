d402fea20c97a2af2d292ca620bf0af1ecaf2a8d
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-04-28 17:15:45 +0200

    react hook form practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useForm} from 'react-hook-form';
 
 function App() {
+
+  const {
+    register,
+    formState: {
+      errors, isValid
+    },
+    handleSubmit,
+    reset
+  } = useForm({
+    mode: "onBlur"
+  });
+
+  const onSubmit = (data) => {
+    console.log(JSON.stringify(data))
+    reset();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type='text' {...register('firstName', {
+          required: "Filed is required",
+          minLength: {
+            value: 5,
+            message: 'Min 5 symbols'
+          }
+        })}/><br/>
+        <div>{errors?.firstName && <p>{errors?.firstName?.message || 'Error'}</p>}</div>
+        <input type='submit' disabled={!isValid}/>
+      </form>
     </div>
   );
 }
